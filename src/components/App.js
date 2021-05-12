31d0aa32f476cf9228630a2e805ded23ac0c65b7
adrianve <adrianve@pm.me>
2021-05-12 10:40:34 -0500

    update to test redux integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,44 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
 import { Route } from 'react-router';
+import { handleInitialData } from '../actions/shared';
 import '../App.css';
 import Login from './Login';
 import Nav from './Nav'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Built with React and Redux
-      </header>
-      <Nav />
-      <Route path='/login' component={Login} />
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    this.props.dispatch(handleInitialData())
+  }
+
+  render(){
+    const {authedUser, users, questions} = this.props
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          Built with React and Redux
+        </header>
+        <Nav />
+        {authedUser}
+        <div>
+          {Object.keys(users).map((id) => <p key={id}>{id}</p>)}
+        </div>
+        <div>
+          {Object.keys(questions).map((id) => <p key={id}>{id}</p>)}
+        </div>
+        <Route path='/login' component={Login} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = ({users, questions, authedUser}) =>{
+  return {
+    authedUser,
+    users,
+    questions
+  }
+}
+
+export default connect(mapStateToProps)(App);
